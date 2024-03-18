4e847b37cd225463463da762498b2877d9e29c10
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2024-03-17 22:47:20 -0700

    updated to work properly

--- cli.py
+++ cli.py
@@ -10,6 +10,18 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def inputs_overview():
+    goals = input("Q1 of 8: Specific Goals?: ")
+    kpis = input("Q2 of 8:Measurable?: ")
+    achieveable = input("Q3 of 8:Achievable?: ")
+    relevance = input("Q4 of 8:Relevance?: ")
+    time_bound = input("Q5 of 8:Time Bound?: ")
+    schedule = input("Q6 of 8:Your Schedule?: ")
+    resources = input("Q7 of 8:Resources?: ")
+    budget = input("Q8 of 8:Budget?: ")
+    return goals, kpis, achieveable, relevance, time_bound, schedule, resources, budget
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generative AI Project Planning Tool")
     parser.add_argument("--create", help="Create a new project plan")
@@ -21,29 +33,38 @@ if __name__ == "__main__":
 
     if args.create:
         logger.info(" Creating a project plan for: %s", args.create)
-        logger.info("************** STEP 1: Project Overview **************")
-        goals = input("Specific Goals?: ")
-        kpis = input("Measurable?: ")
-        achieveable = input("Achievable?: ")
-        relevance = input("Relevance?: ")
-        time_bound = input("Time Bound?: ")
+        logger.info("************** STEP 1: Collect Input **************")
+        goals, kpis, achieveable, relevance, time_bound, schedule, resources, budget = (
+            inputs_overview()
+        )
         prompt_overview = prompts.prompt_overview(
             args.create, goals, kpis, achieveable, relevance, time_bound
         )
         text_overview = gpt.text_generation(prompt_overview)
-        logger.info("************** STEP 2: Creating the Project Plan **************")
-        schedule = input("Your Schedule?: ")
-        resources = input("Resources?: ")
-        budget = input("Budget?: ")
+
         prompt_plan = prompts.prompt_plan(text_overview, schedule, resources, budget)
         text_plan = gpt.text_generation(prompt_plan)
 
         prompt_tickets = prompts.prompt_tickets(text_overview + text_plan)
         text_tickets = gpt.text_generation(prompt_tickets)
 
-        print(text_overview)
-        print(text_plan)
-        print(text_tickets)
+        text_all = text_overview + text_plan + text_tickets
+
+        prompt_text_all = prompts.prompt_writing_critic(text_all)
+        text_all = gpt.text_generation(prompt_text_all)
+
+        prompt_deliverables = prompts.prompt_deliverables(text_all)
+        text_deliverables = gpt.text_generation(prompt_deliverables)
+
+        prompt_learning_resources = prompts.prompt_learning_resources(text_all)
+        text_learnings_resources = gpt.text_generation(prompt_learning_resources)
+
+        text_all = text_all + text_deliverables + text_learnings_resources
+
+        prompt_final = prompts.prompt_writing_critic(text_all)
+        text_final = gpt.text_generation(prompt_final)
+
+        logger.info(text_final)
 
     elif args.view:
         print("Viewing existing project plans...")
--- prompts.py
+++ prompts.py
@@ -5,6 +5,64 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def prompt_learning_resources(plan):
+    """Prompt user for learning resources details"""
+    prompt = f"""
+    
+    Role: you are a technical executor working with a project manager. 
+
+    Task: You will create a list of learning resources for the project plan. Include links to websites, youtube channels, wikipedia pages, and books that will help the team learn the skills they need to complete the project.
+    
+    Here is the learning plan you will use and add resources to at the bottom: 
+
+    {plan}
+
+    Style: Use Markdown to format the document.
+
+    I don't want your thoughts, just the revised text with your improvements.
+
+    """
+    logger.info("Learning resources prompt generated")
+    return prompt
+
+
+def prompt_deliverables(project_plan):
+    """Prompt user for project deliverables details"""
+    prompt = f"""
+    
+    Role: you are a certified project manager. 
+
+    Task: You will create a list of deliverables for this project, based on the project plan. provided here:
+
+    {project_plan}
+
+    Content: You will write a list of deliverables for a project. 
+
+    Style: Use Markdown to format the document.
+
+     I don't want your thoughts, just the revised text with your improvements.
+
+    """
+    logger.info("Deliverables prompt generated")
+    return prompt
+
+
+def prompt_writing_critic(text):
+    """Prompt user for writing critic details"""
+    prompt = f"""
+    
+    Role: you are a writing critic. 
+
+    Task: You will rewrite the following text to make it more engaging and informative.:
+    {text}
+
+    I don't want your thoughts, just the revised text in it's entirety with your improvements.
+
+    """
+    logger.info("writing critic prompt generated")
+    return prompt
+
+
 def prompt_overview(name, goals, kpis, achieveable, relevance, time_bound):
     """Prompt user for project overview details"""
 
@@ -24,10 +82,12 @@ def prompt_overview(name, goals, kpis, achieveable, relevance, time_bound):
 
     Style: I need the following sections: Introduction, Goals, Scope, and Stakeholders. Use Markdown to format the document.
 
-    You can make some logical assumptions about the project based on the information I provide you
+    You can make some logical assumptions about the project based on the information I provide you.
+
+    I don't want your thoughts, just the revised text with your improvements.
 
     """
-    logger.info("Prompt Overview: %s", prompt)
+    logger.info("Executive summary prompt generated")
     return prompt
 
 
@@ -50,26 +110,33 @@ def prompt_plan(overview, schedule, resources, budget):
     Style: Use Markdown to format the document.
 
     Remember to make logical assumptions about the project based on the information I provide you. Use your skills and impress me.
+
+     I don't want your thoughts, just the revised text with your improvements.
+
    
     """
-    logger.info("Prompt Overview: %s", prompt)
+    logger.info("Planning prompt generated")
     return prompt
 
 
-def prompt_tickets(plan):
+def prompt_tickets(existing_plan):
     """Prompt user for project tickets"""
 
-    prompt = """
+    prompt = f"""
     
-    Role: you need to be a technical executor working with a project manager. You need to take the project plan and create detailed task for Jira tickets.
+    Role: you need to be a technical executor working with a project manager. You need to take the project plan and create detailed tasks for Jira tickets.
 
-    Your Task: Create Jira tickets for the project plan. Estimate the time and resources (human and otherwise) required for each task. Organize the tasks by features, stories, and tasks.
+    Your Task: Create Jira tickets for the project plan. Estimate the time and resources (human and otherwise) required for each task. 
+   
 
-    Here is the plan: {plan}
+    Here is the plan: {existing_plan}
 
     Style: Use Markdown to format the document.
 
-    Remember to make organize your response into features, stories, and tasks. Use your skills and impress me.
+    Use your skills and impress me with how you create clear and concise tasks
+
+     I don't want your thoughts, just the revised text.
+
     """
-    logger.info("Prompt Tickets: %s", prompt)
+    logger.info("Jira tickets prompt generated")
     return prompt
